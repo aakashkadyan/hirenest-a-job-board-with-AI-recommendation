d20bb557f28038510a7d3e4f65f80b33f6725bc9
Aakash Kadiyan <45416307+aakashkadyan@users.noreply.github.com>
2025-06-09 19:33:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,14 +19,16 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://hirenest-a-job-board-with-ai-recomm.vercel.app/'],
-  credentials: true
+  origin: ['http://localhost:5173', 'https://hirenest-a-job-board-with-ai-recomm.vercel.app'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.options('*', cors());
 
 
 // Middleware
-app.use(express.json());
+
 app.use(bodyParser.json());
 app.use(express.static('static'));
 
